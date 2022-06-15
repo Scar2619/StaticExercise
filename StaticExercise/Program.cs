4ffefdbfde2d589a4d67f00f5e51ba4903fb2a0f
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-15 11:05:35 -0400

    final

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(60);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(25);
+
+            Console.WriteLine($"After conversion: Celsius is {celsius} and "
+                + $"Fahrenheit is {fahrenheit}.");
+
+        }
+    }
+}
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		public static double FahrenheitToCelsius(double temp)
+		{
+			double f = temp;
+			return (f - 32) * 5 / 9;
+		}
+
+		public static double CelsiusToFahrenheit(double temp)
+        {
+			double c = temp;
+			return c * 9 / 5 + 32;
+        }
+
+	}
+}
+
